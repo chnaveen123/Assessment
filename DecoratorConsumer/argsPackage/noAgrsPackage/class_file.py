ceaac05ec046b2ed7ebad965003de19c08606a3f
chnaveen123 <103917317+chnaveen123@users.noreply.github.com>
2022-04-19 10:56:56 +0530

    Update class_file.py

--- DecoratorConsumer/argsPackage/noAgrsPackage/class_file.py
+++ DecoratorConsumer/argsPackage/noAgrsPackage/class_file.py
@@ -1,2 +1,7 @@
 
-from noAgrsPackage import class_file as dwa  # class decorator
+def StartStopwitharguement(name1,name2,name3):
+    def outer_fun(fun):
+        def inner_fun(*args):
+            fun(*args)
+        return inner_fun
+    return outer_fun
